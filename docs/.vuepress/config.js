063038ac4c9d0dc00b37b520ab159d057153c7ca
SCIPLUS\yyding <yahya_dyy@163.com>
2021-08-13 14:08:24 +0800

    路由问题1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,20 +11,30 @@ module.exports = {
                 link: '/'
             },
             {
-                text: 'js问题',
-                children: [
-                    {
-                        text: '垃圾回收',
-                        link: '/js/README.md',
-                        activeMatch: '/',
-                    },
-                    {
-                        text: '创建对象',
-                        link: '/js/Object.md',
-                        activeMatch: '/',
-                    },
-                ]
+                text: '垃圾回收',
+                link: '/js/README.md',
+                activeMatch: '/',
             },
+            {
+                text: '创建对象',
+                link: '/js/Object.md',
+                activeMatch: '/',
+            },
+            // {
+            //     text: 'js问题',
+            //     children: [
+            //         {
+            //             text: '垃圾回收',
+            //             link: '/js/README.md',
+            //             activeMatch: '/',
+            //         },
+            //         {
+            //             text: '创建对象',
+            //             link: '/js/Object.md',
+            //             activeMatch: '/',
+            //         },
+            //     ]
+            // },
         ],
         sidebar: [
             // SidebarItem
